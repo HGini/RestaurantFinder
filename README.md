1ff7a6ee81b873cab155becc92543fabf64e6803
HGini <phgini@gmail.com>
2017-10-31 10:17:54 +0530

    First code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # RestaurantFinder
+
+You can use the file 'primer-dataset.json' for importing the documents for the collection.
---
+++ model/db.js
@@ -0,0 +1,5 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/nodewebappdb', {
+  useMongoClient: true,
+  /* other options */
+});
